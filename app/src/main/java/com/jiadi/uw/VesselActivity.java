c8d312e289009484ae6834ab7a19368ce04c59f5
bgpaner <1021792999@qq.com>
2017-07-01 20:39:17 +0800

    UW
    first add

--- app/src/main/java/com/jiadi/uw/VesselActivity.java
+++ app/src/main/java/com/jiadi/uw/VesselActivity.java
@@ -5,29 +5,35 @@ package com.jiadi.uw;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 
-public class VesselActivity extends AppCompatActivity {
+public class VesselActivity extends Fragment {
     private ImageButton buttonCus1;
     private Context mContext;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vessel);
-        mContext=this;
-        buttonCus1= (ImageButton)findViewById(R.id.ButtonCus1);
-        buttonCus1.setOnClickListener(buttonCus1ClickListener);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_vessel, container, false);
+        bindView(view);
+        return view;
     }
 
-    private OnClickListener buttonCus1ClickListener = new OnClickListener() {
+    private void bindView(View view) {//buttonCus1= (ImageButton) buttonCus1.findViewById(R.id.ButtonCus1);
+    }
+
+    /*private OnClickListener buttonCus1ClickListener = new OnClickListener() {
         public void onClick(View arg0) {
             Intent setIntent = new Intent();
             setIntent.setClass(mContext, Wifisetting.class);
             startActivity(setIntent);
         }
-    };
+    };*/
 }
